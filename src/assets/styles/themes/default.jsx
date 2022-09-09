d0b894f6e115cc83950711a862f2fb05d140453b
coutinho08 <mateuscouto98@outlook.com>
2022-09-08 22:15:02 -0300

    modal 70% created

--- src/assets/styles/themes/default.jsx
+++ src/assets/styles/themes/default.jsx
@@ -7,11 +7,15 @@ export default {
       main: '#5061FC',
       dark: '#3346F0',
     },
-
     gray: {
       900: '#222222',
       200: '#BCBCBC',
       300: '#A1A1A1',
+    },
+    danger: {
+      light: '#F97171',
+      main: '#FC5050',
+      dark: '#F63131',
     },
   },
 };
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,20 +1,29 @@
+import PropTypes from 'prop-types';
 import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
-        <h1>Title</h1>
+      <Container danger={danger}>
+        <h1>title</h1>
         <p>body of modal</p>
 
         <Footer>
 
           <button type="button" className="cancel-button">cancel</button>
-          <Button type="button">register</Button>
+          <Button type="button" danger={danger}>delete</Button>
         </Footer>
       </Container>
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
