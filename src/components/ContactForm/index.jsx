4f70a491a3b471664b1b3982d08e51ac707a93e1
coutinho08 <mateuscouto98@outlook.com>
2022-09-18 05:04:56 -0300

    validation name and email with regex

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,31 +1,106 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
+
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
-import Input from '../input';
+import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  console.log(errors);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState, {
+          field: 'name', message: 'name is requried.',
+        },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'name',
+      ));
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const emailAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (emailAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState, {
+          field: 'email', message: 'email is requried.',
+        },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'email',
+      ));
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="name" />
-      </FormGroup>
-
-      <FormGroup
-        error="invalid email"
-      >
-        <Input placeholder="email" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder="phone" />
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={handleNameChange}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        {/*  /* error="invalid email" */}
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="phone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">categories</option>
           <option value="instagram">instagram</option>
+          <option value="discord">discord</option>
+          <option value="youtube">youtube</option>
         </Select>
       </FormGroup>
 
