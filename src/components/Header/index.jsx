0c73d606172b90533dd0b4e81e0090205e3285d7
Mateus Couto <mateuscouto98@outlook.com>
2023-06-25 15:02:31 -0300

    other logo added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/images/logo.svg';
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt="MyContacts" width="201" />
+      <img src={logo} alt="MyContacts" width="250" />
 
     </Container>
   );
